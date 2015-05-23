efe6602bce1b039aead6a01b359e2d61b162ea0b
Andy Sponring <asponring@gmail.com>
2015-05-23 15:14:03 -0700

    Added form elements to main page via a react view nested inside another react view.
    Also added css styles

--- client/views/mealQuery.jsx
+++ client/views/mealQuery.jsx
@@ -1,16 +1,19 @@
-var SearchBar = React.createClass({
+var MealQuery = React.createClass({
   render: function() {
+    // var name = this.props.name;
     return (
       <form>
-        <input type="text" placeholder="Search..." />
-        <p>
-          <input type="checkbox" />
-          {' '}
-          Only show products in stock
-        </p>
+        <input type="number" placeholder="Enter number of meals" />
+      
+        <label><input type="checkbox" id="vegetarian" name="vegetarian" value="vegetarian" class="checkbox" /> Vegetarian</label>
+        <label><input type="checkbox" id="vegan" name="vegan" value="vegan" class="checkbox" /> Vegan</label>
+        <label><input type="checkbox" id="nut-free" name="nut-free" value="nut-free" class="checkbox" /> Nut Free</label>
+        <label><input type="checkbox" id="gluten-free" name="gluten-free" value="gluten-free" class="checkbox" /> Gluten Free</label>
+        <label><input type="checkbox" id="carnivore" name="carnivore" value="carnivore" class="checkbox" /> Carnivore</label>
+        <input type="submit" />
       </form>
     );
   }
 });
 
-React.render(<SearchBar />, document.body);
+// React.render(<SearchBar />, document.body);
