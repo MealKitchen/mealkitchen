e71dfc7eeda7599fb40822336ddb111ecb58fa2b
Melanie Gin <melaniegin1@gmail.com>
2015-05-27 19:38:37 -0700

    update mealPlan model for saving to DB, still working on rendering ingredientList

--- client/views/reviewMeals.jsx
+++ client/views/reviewMeals.jsx
@@ -42,32 +42,32 @@ var ReviewMeals = React.createClass({
 
   //TODO: Send the state to a backbone model to be sent to Yummly
   handleSubmit: function(e){
-    //TODO: save a MealPlan model to a user's mealplan collection, and to the db
-    var that = this;
+
+    // extract ingredients from each recipe to save in mealPlanModel
+    var ingredients = [];
+    for (var i = 0; i < this.props.recipes.length; i++) {
+      ingredients.push(this.props.recipes.at(i).get("ingredients"));
+    }
+    ingredients = ingredients.join().split(',');
+    
+    console.log('ingredients', ingredients);
 
     var mealPlan = new MealPlanModel({
       query: this.props.query,
-      recipes: this.props.recipes
+      recipes: this.props.recipes,
+      ingredientsList: ingredients
     });
-    console.log('mealPlan: ', mealPlan);
+    this.props.onSubmit(ingredients);
+
     mealPlan.save({}, {
       success: function(model, res) {
         console.log("Meal plan saved! Response from server:", res);
-        
-        // extract ingredients from each recipe to save in app state
-        var ingredients = [];
-        for (var i = 0; i < res.length; i++) {
-          ingredients.push(res[i].ingredients);
-        }
-        ingredients = ingredients.join().split(',');
-        that.props.onSubmit(ingredients);
 
       },
       error: function(model, err) {
         console.error("model:", model, "There was an error with your request! ", err);
       }
     });
-
   },
 
   // dynamically render recipes on page according to RecipesCollection
