5fdb538cbd84b7e630358f32a2c685cc74c607ba
Zack Fischmann <zacharyrobert@gmail.com>
2015-05-28 14:47:54 -0700

    implement add checked class to ingredient onclick method in shoppinglist view

--- client/views/ShoppingList.jsx
+++ client/views/ShoppingList.jsx
@@ -8,32 +8,36 @@ var ShoppingList = React.createClass({
 
   componentDidMount: function () {
     var that = this;
-
-    that.listenTo(this.props.mealPlan, 'change', function () {
+    this.listenTo(this.props.mealPlan, 'change', function () {
       console.log('change registered!');
       that.forceUpdate();
     });
   },
 
-  removeIngredient: function (event) {
-    var ingredientsList = this.props.mealPlan.get("ingredientsList").slice();
-    var id = event.target.dataset.id;
-    ingredientsList.splice(id, 1);
-    this.props.mealPlan.set({ingredientsList: ingredientsList});
-  },
+  //This code exists to remove ingredients from the view altogether.
 
+  // removeIngredient: function (event) {
+  //   var ingredientsList = this.props.mealPlan.get("ingredientsList").slice();
+  //   var id = event.target.dataset.id;
+  //   ingredientsList.splice(id, 1);
+  //   this.props.mealPlan.set({ingredientsList: ingredientsList});
+  // },
+
+  //TODO: Refactor to persist checked class after refresh. Currently only adds checked class temporarily.
   crossout: function (event) {
     console.log(event.target.dataset.id);
+    React.findDOMNode(this.refs[event.target.dataset.id]).classList.add("checked");
   },
 
   render: function () {
     var that = this;
+    console.log(this.props);
     return (
       <div>
       {that.props.mealPlan.get('ingredientsList').map(function(ingredient, i) {
         return [
-          <button data-id={i} onClick={that.removeIngredient}>X</button>,
-          <div data-id={i}>{ingredient}</div>
+          <button data-id={i} onClick={that.crossout}>X</button>,
+          <div ref={i} data-id={i}>{ingredient}</div>
           ]
         })
       }
