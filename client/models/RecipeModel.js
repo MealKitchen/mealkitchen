de7e0c72e78c55c9c989cba729e558a6ec1a1efd
Melanie Gin <melaniegin1@gmail.com>
2015-05-30 17:00:07 -0700

    setup recipePreference to save to db, and recipePreference model in Backbone

--- client/models/RecipeModel.js
+++ client/models/RecipeModel.js
@@ -1,6 +1,10 @@
 //TODO: get queryObject from mealQuery view and store in QueryModel
 var RecipeModel = Backbone.Model.extend({
 
+<<<<<<< HEAD
   url: 'api/recipes'
+=======
+  url: 'api/user/preferences'
+>>>>>>> setup recipePreference to save to db, and recipePreference model in Backbone
 
 });
