8af0a5eac9df7d04c3fb67568ba5d76ade311293
Andy Sponring <asponring@gmail.com>
2015-06-05 14:57:42 -0700

    Trying to wrap require config statement in try/catch

--- server/recipe/recipeController.js
+++ server/recipe/recipeController.js
@@ -1,6 +1,15 @@
 var Promise = require('bluebird');
-var appId = process.env.APPLICATION_ID || require('../config/config.js').APPLICATION_ID;
-var apiKey = process.env.APPLICATION_KEY || require('../config/config.js').APPLICATION_KEY;
+
+var appId, apiKey;
+
+try {
+  appId = process.env.APPLICATION_ID || require('../config/config.js').APPLICATION_ID
+  apiKey = process.env.APPLICATION_KEY || require('../config/config.js').APPLICATION_KEY;
+} 
+catch (e) {
+  appId = 12345;
+  apiKey = 98765;
+}
 var http = require('http');
 var Recipe = require('./recipeModel');
 var RecipePreference = require('../recipePreference/recipePreferenceModel');
