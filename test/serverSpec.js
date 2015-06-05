a48612d25d79e96f2271b242d938a919364d338c
Andy Sponring <asponring@gmail.com>
2015-06-05 10:45:52 -0700

    Working test stub for server

--- test/serverSpec.js
+++ test/serverSpec.js
@@ -1,7 +1,21 @@
 var expect = require('../node_modules/chai/chai').expect;
+var request = require('request');
+var app = require('../server/server.js');
 
-describe('Testing', function() {
-  it('should run a test', function() {
-    expect(true).to.equal(true);
+var port = process.env.PORT || 3000;
+
+describe('Node server basic functionality', function() {
+
+  before(function() {
+      app.listen(port);
+  });
+
+  it('Should respond with a 200 status code', function(done) {
+    request
+      .get('http://127.0.0.1:3000/')
+      .on('response', function(response) {
+        expect(response.statusCode).to.equal(200);
+        done();
+      });
   });
 });
