37235ea5968d56311efd4b0cd2a7d8796da72cc1
Melanie Gin <melaniegin1@gmail.com>
2015-06-05 17:33:05 -0700

    merge conflicts

--- client/models/RecipeModel.js
+++ client/models/RecipeModel.js
@@ -1,10 +1,6 @@
 //TODO: get queryObject from mealQuery view and store in QueryModel
 var RecipeModel = Backbone.Model.extend({
 
-<<<<<<< HEAD
   url: 'api/recipes'
-=======
-  url: 'api/user/preferences'
->>>>>>> setup recipePreference to save to db, and recipePreference model in Backbone
 
 });
