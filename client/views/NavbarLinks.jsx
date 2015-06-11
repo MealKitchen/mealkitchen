90580036121d45c9f21b5265434ff5a1cc8c1423
Zack Fischmann <zacharyrobert@gmail.com>
2015-06-10 19:06:23 -0700

    update navbar classes

--- client/views/NavbarLinks.jsx
+++ client/views/NavbarLinks.jsx
@@ -5,9 +5,9 @@ var NavbarLinks = React.createClass({
   render : function() {
     return (
       <ul className="nav navbar-nav">
-        <li><a data-route="/mealquery" onClick={this.props.linkHandler}>Create Meal Plan</a></li>
-        <li><a data-route="/mealplans" onClick={this.props.linkHandler}>View Meal Plans</a></li>
-        <li><a data-route="/logout" onClick={this.props.logOut}>Log Out</a></li>
+        <li><a className="navbar-link" data-route="/mealquery" onClick={this.props.linkHandler}>Create Meal Plan</a></li>
+        <li><a className="navbar-link" data-route="/mealplans" onClick={this.props.linkHandler}>View Meal Plans</a></li>
+        <li><a className="navbar-link" data-route="/logout" onClick={this.props.logOut}>Log Out</a></li>
       </ul>
     );
   }
